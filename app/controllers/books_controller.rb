2b5cd1482faf170d71748ac94779bfc8bb588dc1
sayabe22 <sayabe22@gmail.com>
2024-08-04 14:01:37 +0000

    [Add]books#show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,10 +12,29 @@ class BooksController < ApplicationController
     @books = Book.all
   end 
   
+  def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(books_params)
+    redirect_to books_path
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
   
   
-  
-  
+  private
   
   def books_params
     params.require(:book).permit(:title, :body)
