6b038cb0d26f503989c59ecbe860aaaddb2986a1
sayabe22 <sayabe22@gmail.com>
2024-08-05 02:51:17 +0000

    [Add]user_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end 
+  
   private
+  
   def user_params
-    params.require(:user).permit(:title, :body, :image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about',as: 'about'
   
   resources :books
-  resources :users, only: [:new, :show, :edit]
+  resources :users, only: [:new, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
