f3a87df90d083395ae4a2e5788321b9f6fad04f8
sayabe22 <sayabe22@gmail.com>
2024-08-06 03:00:41 +0000

    [Add]レイアウト変更1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,13 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end 
   
   def show
     @book = Book.find(params[:id])
     @books = Book.all
+    @user = @book.user
   end
   
   def edit
